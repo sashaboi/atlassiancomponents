5d4a9eb9dd9852eb58aa25b51d1567d51ea5f069
sashaboi <onkarcool123@gmail.com>
2022-10-18 23:18:27 +0530

    feat: navigation basics

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { AtlassianIcon, AtlassianLogo } from "@atlaskit/logo";
+
+import {
+  AtlassianNavigation,
+  PrimaryButton,
+  PrimaryDropdownButton,
+  ProductHome,
+  Create,
+  Settings,
+} from "@atlaskit/atlassian-navigation";
+import DropdownMenu, {
+  DropdownItem,
+  DropdownItemGroup,
+} from "@atlaskit/dropdown-menu";
+const AtlassianProductHome = () => (
+  <ProductHome icon={AtlassianIcon} logo={AtlassianLogo} />
+);
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [dropdown, setDropdown] = useState();
+  const CreateButton = () => (
+    <Create
+      buttonTooltip="Create a new page"
+      iconButtonTooltip="Create a new page"
+      text="Create"
+      href="#"
+      onClick={() => {}}
+    />
+  );
+  const DefaultSettings = () => <Settings tooltip="Product settings" />;
+  return (
+    <div>
+      <AtlassianNavigation
+        label="site"
+        primaryItems={[
+          <PrimaryButton onClick={() => alert("Hello")}>Alert</PrimaryButton>,
+          <PrimaryDropdownButton onClick={() => setDropdown((prev) => !prev)}>
+            <DropdownMenu isOpen={dropdown} trigger="Page actions">
+              <DropdownItemGroup>
+                <DropdownItem description="Previous versions are saved">
+                  Edit
+                </DropdownItem>
+                <DropdownItem>Move</DropdownItem>
+                <DropdownItem>Clone</DropdownItem>
+              </DropdownItemGroup>
+            </DropdownMenu>
+          </PrimaryDropdownButton>,
+          <PrimaryDropdownButton>Projects</PrimaryDropdownButton>,
+          // <DropDown />,
+        ]}
+        renderCreate={CreateButton}
+        renderProductHome={AtlassianProductHome}
+        renderSettings={DefaultSettings}
+      />
+    </div>
+  );
 };
 
 export default Navbar;
